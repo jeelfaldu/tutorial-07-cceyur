5497027f37c89dbb6b6e1452587e48f1ec78bd24
jeelfaldu <faldujeel@gmail.com>
2020-03-05 23:01:48 +0530

    2 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+
 import { ContentComponent } from './content/content.component';
 import { ProjectComponent } from './project/project.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,7 +17,6 @@ import { StudentdetailsComponent } from './studentdetails/studentdetails.compone
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     ContentComponent,
     SignupComponent,
     ProjectComponent,
